1ef016325762d0af45852afab35b816bc320992c
Jiahao Liu <jiahaoliuliu@gmail.com>
2014-04-21 15:41:55 +0200

    Added simple log

--- src/com/jiahaoliuliu/autocompletetextviewhack/MainActivity.java
+++ src/com/jiahaoliuliu/autocompletetextviewhack/MainActivity.java
@@ -7,6 +7,7 @@ import com.jiahaoliuliu.autocompletetextviewhack.model.Cinema;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +16,8 @@ import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends Activity {
+
+	private static final String TAG = "AutoCompleteTextViewHack";
 
 	// Layout
 	private AutoCompleteTextView simpleAutoCompleteTextView;
@@ -25,6 +28,29 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
+		Log.v(TAG, "Started");
 		simpleAutoCompleteTextView = (AutoCompleteTextView) findViewById(R.id.simpleAutoCompleteTextView);
 		detailsTextView = (TextView) findViewById(R.id.detailsTextView);
 
